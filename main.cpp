4ecfc83123c2da2dac2e48613ce5dee4f6baf90f
Sm1the <apxahreji123@gmail.com>
2020-04-17 22:30:42 +0700

    Write code for start program

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
-#include <iostream>
-
+#include <cstdio>
+#include "board_start.h"
+#include "board_print_plain.h"
+#include "boardgenerate.h"
 int main() {
-
+    printf("Welcome to the game \"Chess\".\n"
+           "To do at least, enter in the console \"start position of the turn and end position of the move\"\n"
+           "Be sure to indicate the sequence through the dash.\n");
+    char deck[8][8];
+    board_start(deck);
+    while (1) {
+        printf("\n");
+        board_print_plain(deck);
+        board(deck);
+    }
 }
